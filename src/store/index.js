f6f06494fd983cb8c7f094c93192a14c01bc43f5
robinluyi <robinluyi@gmail.com>
2020-11-08 21:33:43 +0800

    lession1

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore } from 'vuex'
-
+import state from './state'
+import actions from './actions'
+import mutations from './mutations'
+import user from './module/user'
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  mutations,
+  actions,
   modules: {
+    user
   }
 })
